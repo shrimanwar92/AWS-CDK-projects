ab1269b68eca1c5b2e274b9737544fbc513604a6
Nilay Shrimanwar <shrimanwar92@gmail.com>
2021-07-14 22:15:51 +0530

    Update utils.ts

--- aurora-ecs/lib/utils.ts
+++ aurora-ecs/lib/utils.ts
@@ -1,5 +1,5 @@
 export const DATABASE_PORT = 5432;
 export const CONTAINER_PORT = 80;
 export const DEFAULT_REPOSITORY_NAME = 'server';
-export const DATABASE_NAME = 'nimbusdb';
+export const DATABASE_NAME = 'db';
 export const AURORA_ENGINE_VERSION = '10.12';
